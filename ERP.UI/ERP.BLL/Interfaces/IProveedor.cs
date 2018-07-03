1652555de4ac4ae6773cf5c1349ad6c4d8d3510d
Sofi <sofi.tenom31@gmail.com>
2018-07-02 22:15:45 -0600

    Tbl_Proveedor
    CRUD Proveedor - Sofia

---
+++ ERP.UI/ERP.BLL/Interfaces/IProveedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.DATA;
+
+namespace ERP.BLL.Interfaces
+{
+    public interface IProveedor
+    {
+        List<Proveedor> ListarProveedor();
+        Proveedor BuscarProveedoro(int Id_Proveedor);
+        void InsertarProveedor(Proveedor proveedor);
+        void ActualizarProveedor(Proveedor proveedor);
+        void EliminarProveedor(int Id_Proveedor);
+    }
+}
