f374e87313b4c8de5fb55872c6d0a8b9ba7326be
Sofi <sofi.tenom31@gmail.com>
2018-10-30 14:06:01 -0600

    Actualizacion

--- ERPSIL_be/api/contrib/erpsil_contabilidad/erpsil_contabilidad.php
+++ ERPSIL_be/api/contrib/erpsil_contabilidad/erpsil_contabilidad.php
@@ -1,15 +1,15 @@
 <?php
 
-
 function mostrarContabilidad(){
-|/*ignorar esto
-    $sql= "INSERT INTO `tbl_contabilidad` (`total_factura`, `total_pagar`, `total_planilla`)
-     values ((select Sum(`total`) from `tbl_factura`),(select Sum(`pago`) from `tbl_pagos`),(select Sum(`salario_neto`) from `tbl_planilla`));"
-    /*$q = "SELECT top 1 * FROM `tbl_contabilidad`
-    order by `id_contabilidad`";*/ 
 
-     $q = "SELECT * FROM `tbl_contabilidad`";
+    //$total_factura= "SELECT Sum(`total`) FROM `tbl_factura`";
+    //$total_pagos= "SELECT Sum(`pago`) FROM `tbl_pagos`";
+    //$total_planilla= "SELECT Sum(`salario_neto`) FROM `tbl_planilla`";
+    //$sql= "INSERT INTO `tbl_contabilidad` (`total_factura`, `total_pagar`, `total_planilla`)
+    //VALUES ('$total_factura',4,5)";
+    //db_query($sql,0);
+    $q = "SELECT * FROM `tbl_contabilidad` ORDER BY id_contabilidad DESC LIMIT 1";
 
-    return db_query($q, 2);
+    return db_query($q,2);
 
 }
