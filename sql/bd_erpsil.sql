bb0be725ab602953ee847002213150ae27882d3d
Sofi <sofi.tenom31@gmail.com>
2018-11-22 19:14:49 -0600

    Update bd_erpsil.sql

--- sql/bd_erpsil.sql
+++ sql/bd_erpsil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-11-2018 a las 21:49:20
+-- Tiempo de generación: 23-11-2018 a las 02:14:21
 -- Versión del servidor: 10.1.34-MariaDB
 -- Versión de PHP: 7.2.7
 
