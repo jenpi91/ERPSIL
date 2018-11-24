b64103ecad2bc1023826a5c256ff059baf1e3b7f
Daniel Olsen <danielolsen@Tito.local>
2018-11-24 17:50:16 -0600

    Arrelo de conflicto

--- ERPSIL_be/api/contrib/erpsil_contabilidad/erpsil_contabilidad.php
+++ ERPSIL_be/api/contrib/erpsil_contabilidad/erpsil_contabilidad.php
@@ -4,7 +4,7 @@ function mostrarContabilidad(){
 
     $insertar= "INSERT INTO `tbl_contabilidad` (`total_factura`, `total_pagar`, `total_planilla`, `total`, `total_cliente`, `total_articulos_vendidos`)
     VALUES ((select * from suma_facturas),(select * from suma_pagos),(select * from suma_planilla)
-    ,(select * from suma_planilla+select * from suma_pagos),(select * from contar_clientes),(select * from contar_articulos))";
+    ,((select * from suma_planilla) + (select * from suma_pagos)),(select * from contar_clientes),(select * from contar_articulos))";
     db_query($insertar,0);
     $q = "SELECT * FROM `tbl_contabilidad` ORDER BY id_contabilidad DESC LIMIT 1";
 
